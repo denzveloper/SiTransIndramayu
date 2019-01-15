9f4a2e2fe32e65e164b52ae7a5cc93939319aa99
DENZVELOPER - DzEN/DzEN <dandyoctavian@gmail.com>
2019-01-16 01:04:33 +0700

    remove comment

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -13,19 +13,14 @@ class Login extends CI_Controller {
 
     public function index(){
     	if($this->loginm->chksess()){
-			//jika memang session sudah terdaftar, maka dialihkan ke halaman dahsboard
 			redirect("dashboard");
 		}else{
-			//set form validation
 			$this->form_validation->set_rules('mail', 'Email', 'required|valid_email|trim|max_length[128]');
 			$this->form_validation->set_rules('pass', 'Password', 'required|max_length[64]');
-			//set message form validation
 			$this->form_validation->set_message('required', 'harus diisi!');
-			//check Validation
 			if ($this->form_validation->run() == TRUE){
 				$mail = $this->input->post("mail", TRUE);
 				$pass = $this->input->post("pass", TRUE);
-				//check user mail and password
 				$cek = $this->loginm->login($mail, $pass);
 				if ($cek != FALSE){
 					foreach ($cek as $hit){
@@ -37,7 +32,6 @@ class Login extends CI_Controller {
 						);
 						$fnam = $hit->namadepan;
 					}
-					//set session userdata
 					$this->session->set_userdata($sesar);
 					$msg[] = array('ico' => 'ti-check', 'txt' => "<b>Hi, $fnam!</b><br><i> Welcome to Admin Page.</i>", 'typ' => 'success');
 					$this->session->set_flashdata('info', $msg);
